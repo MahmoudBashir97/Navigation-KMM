0eebddd330bbc5aa0a172eb19cbe83c8c2fd7aec
Mahmoud Bashir <mbashir@whitehelmet.sa>
2024-03-31 00:17:33 +0200

    Navigation-Voyager . Work around Tab navigation with bottom Nav.

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,10 +1,22 @@
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.tab.CurrentTab
+import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabNavigator
 import cafe.adriel.voyager.transitions.SlideTransition
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import screen.home.HomeScreen
+import tab.home.HomeTab
+import tab.profile.ProfileTab
+import tab.settings.SettingsTab
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
@@ -12,8 +24,35 @@ import screen.home.HomeScreen
 fun App() {
     MaterialTheme {
         // start destination
-        Navigator(HomeScreen()){
-            SlideTransition(it)
+        // default navigation
+        /* Navigator(HomeScreen()){
+             SlideTransition(it)
+         }*/
+
+        // bottomNavigation
+        TabNavigator(HomeTab) {
+            Scaffold(
+                bottomBar = {
+                    BottomNavigation {
+                        TabNavigationItem(HomeTab)
+                        TabNavigationItem(ProfileTab)
+                        TabNavigationItem(SettingsTab)
+                    }
+                }
+            ) {
+                CurrentTab()
+            }
         }
     }
+}
+
+@Composable
+private fun RowScope.TabNavigationItem(tab: Tab) {
+    val tabNavigator = LocalTabNavigator.current
+    BottomNavigationItem(
+        selected = tabNavigator.current == tab,
+        onClick = { tabNavigator.current = tab },
+        label = { Text(tab.options.title) },
+        icon = {}
+    )
 }
